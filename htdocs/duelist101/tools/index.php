6150115485ef95ded3f583432746fae93f524036
Nick Steimling <nick.steimling@gmail.com>
2014-06-17 00:14:01 -0500

    Added slim/views for StampTE

--- htdocs/duelist101/tools/index.php
+++ htdocs/duelist101/tools/index.php
@@ -1,21 +1,20 @@
 <?php
+error_reporting(E_ALL);
 require '../wp-load.php';
 require 'tools-config.php';
 require APP_DIR . 'vendor/autoload.php';
 require APP_DIR . 'vendor/redbean/rb.php';
+require APP_DIR . 'vendor/stampte/StampTE.php';
 
 R::setup('sqlite:' . DB_FILE );
 $app = new \Slim\Slim(array(
-    'view' => new \Slim\Views\Twig(),
-    'templates.path' => TEMPLATES_DIR,
+    'view' => new \Slim\Views\StampView()
 ));
-$app->view()->parserExtensions = array(
-    new \Slim\Views\TwigExtension(),
-);
 
 $app->get('/reagents/', function () use ($app) {
+	$template = file_get_contents( TEMPLATES_DIR . 'stamp-reagents.html' );
 	$reagents = R::find( 'reagent' );
-	$test = $app->view->fetch('reagent-list.html', array('reagents' => $reagents));
+	$test = $app->view->render($template, $reagents);
 	
 	// Wordpress Print Routing
 	get_header();
