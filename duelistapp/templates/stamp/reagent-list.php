cbf4f39584b1e2b1d4afe95d02d2e4140e1b79dd
Nick Steimling <nick.steimling@gmail.com>
2014-06-17 07:31:25 -0500

    Fixed naming conventions where getIdAndName changed in reagent-list templates. Removed extra code from reagent-single.php template.

--- duelistapp/templates/stamp/reagent-list.php
+++ duelistapp/templates/stamp/reagent-list.php
@@ -7,7 +7,7 @@
 			$cutReagent->injectRaw('lastFooter', '<div style="clear: both;"></div>');
 		}
 		$cutReagent->setName($reagent->name);
-		$cutReagent->setIdAndName( urlencode( $reagent->name ) );
+		$cutReagent->setLinkName( urlencode( $reagent->name ) );
 		$cutReagent->setImage($reagent->image);
 		$cutReagent->setClassName($reagent->class->name);
 		$cutReagent->setRank($reagent->rank);
--- duelistapp/templates/stamp/reagent-single.php
+++ duelistapp/templates/stamp/reagent-single.php
@@ -1,11 +1,7 @@
 <?php
 		$cutReagent = $template->getReagent();
-		if ( $i % 3 == 0 ) {
-			$cutReagent->setLastOption('last');
-			$cutReagent->injectRaw('lastFooter', '<div style="clear: both;"></div>');
-		}
+		
 		$cutReagent->setName($reagent->name);
-		$cutReagent->setIdAndName( urlencode( $reagent->name ) );
 		$cutReagent->setImage($reagent->image);
 		$cutReagent->setClassName($reagent->class->name);
 		$cutReagent->setRank($reagent->rank);
